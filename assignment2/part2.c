b5ea48c948a36e269f45446226bc8ab870dd03f8
lionelhan88 <d.bcooper@hotmail.com>
2019-11-10 23:14:09 -0800

    final push

--- assignment2/part2.c
+++ assignment2/part2.c
@@ -3,11 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
 
 
 void myHandler(int mySignal){
 
-	printf("Child Process: %d waitting - Received signal: %d\n", getpid(),mySignal);
+	printf("Child Process: %d waiting - Received signal: %d\n", getpid(),mySignal);
 	sigset_t set;
 	sigemptyset(&set);
 	sigaddset(&set, SIGUSR1);
@@ -18,22 +20,21 @@ void myHandler(int mySignal){
 
 
 pid_t forkChild(char *command, char **arguments){
-	pid_t pid; 
+	pid_t pid;
 	struct sigaction act;
 	act.sa_handler = &myHandler;
 	sigset_t set;
 	sigemptyset(&set);
 	sigaddset(&set, SIGUSR1);
 	sigprocmask(SIG_BLOCK, &set, NULL);
-	int sig;
 
 	pid = fork();
 	if(pid == -1){
-		printf("Error, failed to fork \n");			
+		printf("Error, failed to fork \n");
 	}else if(pid == 0){
 
 		printf("child pid is %d\n", getpid());
-		sigaction(SIGUSR1,&act,NULL); 	
+		sigaction(SIGUSR1,&act,NULL);
 		execvp( command, arguments);
 		sleep(2);
 		_exit(0);
@@ -44,6 +45,7 @@ pid_t forkChild(char *command, char **arguments){
 void singler(int count, int signal, pid_t *pid){
 	int i;
 	for(i=1; i<=count; i++){
+		printf("receiving signal %d\n", signal);
 		kill(pid[i], signal);
 	}
 }
@@ -55,7 +57,7 @@ int main(int argc, char *argv[]){
 	char* token = NULL;
 	pid_t pid[10];
 	FILE *fp;
-	int count=0, i, j=0, wstatus, countArg = 0, k, a=0;
+	int count=0, i, j=0, wstatus, countArg = 0, a=0;
 
 	if(argc != 2){
 		printf("The program need exact one file to execute, please try again\n");
@@ -74,7 +76,7 @@ int main(int argc, char *argv[]){
 			}
 			count = countArg + 1; 									// count how many arguments each line has in the file
 
-			char *array[count];										
+			char *array[count];
 			for(i=0; i <= count ; i++){
 				array[i] = NULL;									// initialize an array pointer
 			}
@@ -85,31 +87,37 @@ int main(int argc, char *argv[]){
 				token = strtok(NULL, " \r\n");
 			}
 
-		 	pid[a] = forkChild(array[0], array);					// fork child processes	 	
+		 	pid[a] = forkChild(array[0], array);					// fork child processes
 		 	a++;
 			count = 0;
 			countArg = 0;
 			j++;
 		}
-
+		printf("sending signal to pause all the processes!\n");
     	singler(j, SIGUSR1, pid);
    		sleep(3);
+   		printf("sending signal to restart all the processes!\n");
    		singler(j, SIGUSR1, pid);
    		sleep(3);
+   		printf("sending signal to stop all the processes!\n");
    		singler(j, SIGSTOP, pid);
    		sleep(4);
+   		printf("sending signal to continue all the processes!\n");
    		singler(j, SIGCONT, pid);
 		sleep(3);
+		printf("terminating all the processes!\n");
+		singler(j, SIGINT, pid);
+		sleep(3);
 
-		for(int k=0; k<10; k++){
-			kill(pid[k],SIGINT);
-				printf("pid is waiting %d\n", pid[k]);
-		 		waitpid(pid[k], &wstatus, WNOHANG);
+		for(int k=0; k<j; k++){
+
+			printf("pid is waiting %d\n", pid[k]);
+		 	waitpid(pid[k], &wstatus, WNOHANG);
 			}
 
 	}
 	free(text);
 	fclose(fp);
-	
+
 	return 0;
 }
