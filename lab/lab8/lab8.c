a80b9970f6ff00899453ad60d1d1a3cb958730ba
lionelhan <d.bcooper@hotmail.com>
2019-12-06 10:42:45 -0800

    q

--- lab/lab8/lab8.c
+++ lab/lab8/lab8.c
@@ -1,188 +1,186 @@
+/*=============================================================================
+ * Program Name: lab8
+ * Author: Jared Hall
+ * Date: 11/17/2019
+ * Description:
+ *     A simple program that implements a queue of meal tickets
+ *
+ * Notes:
+ *     1. DO NOT COPY-PASTE MY CODE INTO YOUR PROJECTS.
+ *===========================================================================*/
+
+//========================== Preprocessor Directives ==========================
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+//=============================================================================
 
+//================================= Constants =================================
+#define MAXNAME 15
+#define MAXQUEUES 4
+#define MAXTICKETS 3
+#define MAXDISH 20
+//=============================================================================
 
-struct mealTicket{
+//============================ Structs and Macros =============================
+typedef struct mealTicket{
 	int ticketNum;
 	char *dish;
-};
+} mealTicket;
 
-struct MTQ{
-	char *name;
-	struct mealTicket *buffer;
+typedef struct MTQ {
+	char name[MAXNAME];
+	struct mealTicket * const buffer;
 	int head;
 	int tail;
-	int length;
-};
+	const int length;
+} MTQ;
 
-
-int enqueue(char *MTQ_ID, struct mealTicket *MT, struct MTQ *meal){
-	int ticNum = 0;
-	while(meal->buffer[ticNum].dish != NULL){
-	 	ticNum += 1;
-	 	if(ticNum == 4){
-	 		ticNum = 0;
-	 		return 0;
-	 	}
+#define MTQ_DEF(var,id)                                                       \
+	mealTicket var##_buf[MAXTICKETS+1] = {[MAXTICKETS].ticketNum = -1};       \
+	MTQ var = {                                                               \
+	.name = id,                                                               \
+	.buffer = var##_buf,                                                      \
+	.head = 0,                                                                \
+	.tail = 0,                                                                \
+	.length = MAXTICKETS,                                                     \
 	}
-	if(MTQ_ID != meal->name){
-		return 0;
-	}else{
-		meal->buffer[ticNum].dish = MT->dish;
-		meal->buffer[ticNum].ticketNum = MT->ticketNum;
-		meal->tail += 1;
-		return 1;
+	
+
+MTQ *registry[MAXQUEUES];
+int TICKET = 1;
+MTQ_DEF(breakfast, "Breakfast");
+MTQ_DEF(lunch, "Lunch");
+MTQ_DEF(dinner, "Dinner");
+MTQ_DEF(bar, "Bar");
+//=============================================================================
+
+//================================= Functions =================================
+int enqueue(char *MTQ_ID, mealTicket *MT) {
+	int ret = 0; 
+	int i, flag = 0;
+	
+	//Step-1: Find registry
+	for(i=0;i<MAXQUEUES;i++) { 
+		if(strcmp(MTQ_ID, registry[i]->name) == 0) { flag = 1; break; }
 	}
 
-}
-
-
-int dequeue(char *MTQ_ID, struct mealTicket *MT, struct MTQ *meal){
-
-	if(meal->tail == 1){
-		return 0;
-	}
-	for(int i=0; i<meal->tail; i++){
-		if(meal->buffer[i].ticketNum == MT->ticketNum){
-			printf("Queue %s - Ticket Number: %d - Dish: %s \n", meal->name, meal->buffer[i].ticketNum, meal->buffer[i].dish);
-			meal->tail -= 1;
-	 		for(int l=i ; l<meal->tail; l++){
-				meal->buffer[l].ticketNum = meal->buffer[l+1].ticketNum;
-				meal->buffer[l].dish = meal->buffer[l+1].dish;
-			}
-			return 1;
+	//STEP-2: Enqueue the ticket
+	if(flag) {
+		int tail = registry[i]->tail;
+		if(registry[i]->buffer[tail].ticketNum != -1) {
+			MT->ticketNum = TICKET;
+			TICKET++;
+			registry[i]->buffer[tail] = *MT;
+			if(tail == registry[i]->length) { registry[i]->tail = 0; }
+			else { registry[i]->tail++; }
+			ret = 1;
 		}
 	}
-	return 0;
+	return ret;
 }
 
+int dequeue(char *MTQ_ID, int ticketNum, mealTicket *MT) {
+	int ret = 0;
+	int i, flag = 0;
+	
+	//Step-1: Find registry
+	for(i=0;i<MAXQUEUES;i++) { 
+		if(strcmp(MTQ_ID, registry[i]->name) == 0) { flag = 1; break; }
+	}
+	
+	//Step-2: Dequeue the ticket
+	if(flag) {
+		int head = registry[i]->head;
+		int tail = registry[i]->tail;
+		
+		if(head != tail && ticketNum == registry[i]->buffer[head].ticketNum) {
+			//copy the ticket
+			MT->ticketNum = registry[i]->buffer[head].ticketNum;
+			strcpy(MT->dish, registry[i]->buffer[head].dish);
+			
+			//change the null ticket to empty
+			if(head == 0) { 
+				registry[i]->buffer[registry[i]->length].ticketNum = 0;
+			} else { 
+			registry[i]->buffer[head-1].ticketNum = 0;
+			}
+			
+			//change the current ticket to null
+			registry[i]->buffer[head].ticketNum = -1;
+			
+			//increment the head
+			if(head == registry[i]->length+1) { registry[i]->head = 0; }
+			else { registry[i]->head++; }
+			ret = 1;
+		}
+	}
+	return ret;
+}
+//=============================================================================
 
-int main(){
-
-	struct MTQ breakfast, lunch, dinner, bar;
-
-	breakfast.name = "Breakfast";
-	breakfast.head = 0;
-	breakfast.tail = 1;
-	breakfast.length = 3;
-	breakfast.buffer = malloc(breakfast.length * sizeof(struct mealTicket));
-
-	lunch.name = "Lunch";
-	lunch.head = 0;
-	lunch.tail = 1;
-	lunch.length = 3;
-	lunch.buffer = malloc(lunch.length * sizeof(struct mealTicket));
-
-	dinner.name = "Dinner";
-	dinner.head = 0;
-	dinner.tail = 1;
-	dinner.length = 3;
-	dinner.buffer = malloc(dinner.length * sizeof(struct mealTicket));
-
-	bar.name = "Bar";
-	bar.head = 0;
-	bar.tail = 1;
-	bar.length = 3;
-	bar.buffer = malloc(bar.length * sizeof(struct mealTicket));
-
-	struct MTQ *registry[4];
-
+//=============================== Program Main ================================
+int main(int argc, char argv[]) {
+	//Variables Declarations
+	char *qNames[] = {"Breakfast", "Lunch", "Dinner", "Bar"};
+	char *bFood[] = {"Eggs", "Bacon", "Steak"};
+	char *lFood[] = {"Burger", "Fries", "Coke"};
+	char *dFood[] = {"Steak", "Greens", "Pie"};
+	char *brFood[] = {"Whiskey", "Sake", "Wine"};
+	int i, j, t = 1;
+	int test[4];
+	char dsh[] = "Empty";
+	mealTicket bfast[3] = {[0].dish = bFood[0], [1].dish = bFood[1], [2].dish = bFood[2]};
+	mealTicket lnch[3] = {[0].dish = lFood[0], [1].dish = lFood[1], [2].dish = lFood[2]};
+	mealTicket dnr[3] = {[0].dish = dFood[0], [1].dish = dFood[1], [2].dish = dFood[2]};
+	mealTicket br[3] = {[0].dish = brFood[0], [1].dish = brFood[1], [2].dish = brFood[2]};
+	mealTicket ticket = {.ticketNum=0, .dish=dsh};
+	
+	//STEP-1: Initialize the registry
 	registry[0] = &breakfast;
 	registry[1] = &lunch;
 	registry[2] = &dinner;
 	registry[3] = &bar;
+	
+	//STEP-2: Push meal tickets into the queues
+	for(i=0;i<MAXTICKETS;i++) {enqueue(qNames[0], &bfast[i]);}
+	for(i=0;i<MAXTICKETS;i++) {enqueue(qNames[1], &lnch[i]);}
+	for(i=0;i<MAXTICKETS;i++) {enqueue(qNames[2], &dnr[i]);}
+	for(i=0;i<MAXTICKETS;i++) {enqueue(qNames[3], &br[i]);}
 
-	struct mealTicket mt1, mt2, mt3, mt4, mt5, mt6, mt7, mt8, mt9, mt10, mt11, mt12;
-	mt1.ticketNum = 1;
-	mt1.dish = "bread";
-
-	mt2.ticketNum = 2;
-	mt2.dish = "juice";
-
-	mt3.ticketNum = 3;
-	mt3.dish = "toast";
-
-	mt4.ticketNum = 4;
-	mt4.dish = "rice";
-
-	mt5.ticketNum = 5;
-	mt5.dish = "noodle";
-
-	mt6.ticketNum = 6;
-	mt6.dish = "steak";
-
-	mt7.ticketNum = 7;
-	mt7.dish = "turkey";
-
-	mt8.ticketNum = 8;
-	mt8.dish = "soup";
-
-	mt9.ticketNum = 9;
-	mt9.dish = "lamb";
-
-	mt10.ticketNum = 10;
-	mt10.dish = "gin";
-
-	mt11.ticketNum = 11;
-	mt11.dish = "vodka";
-
-	mt12.ticketNum = 12;
-	mt12.dish = "whisky";
-
-	int result;
-
-	result = dequeue("Breakfast", &mt1, registry[0] );
-	if(result == 0 && registry[0]->tail == 1){
-		printf("The queue is empty\n");
-		printf("Test Case A - Result Fail\n");
-	}else{
-		printf("Test Case A - Result Success\n");
+	//STEP-3: Dequeue in round robin fashion
+	printf("========== Dequeuing Tickets ==========\n");
+	for(i=0; i<MAXTICKETS; i++) {
+		if(i != 0) {t = i + 1;}
+		for(j=0; j<MAXQUEUES; j++) {
+			dequeue(qNames[j], t, &ticket);
+			printf("Queue: %s - Ticket Number: %i - Dish: %s\n", qNames[j], ticket.ticketNum, ticket.dish);
+			t = t + 3;
+		}
+		printf("\n");
 	}
+	printf("=======================================\n\n");
+	
+	//Step-4: Testing
+	printf("========== Testing ==========\n");
+	TICKET = 1;
+	test[0] = dequeue(qNames[0], TICKET, &ticket);
+	test[3] = enqueue(qNames[0], &bfast[0]);
+	enqueue(qNames[0], &bfast[0]); 
+	enqueue(qNames[0], &bfast[0]);
+	test[2] = enqueue(qNames[0], &bfast[0]);
+	test[1] = dequeue(qNames[0], 1, &ticket);
 
-
-	result = enqueue("Breakfast", &mt1, registry[0] );
-	if(result == 1){
-	//	printf("Adding meal %s to Breakfast\n", mt1.dish);
-		printf("Test Case D - Result Success\n");
-	}else{
-		printf("Test Case D - Result Fail\n");
-	}
-	enqueue("Breakfast", &mt2, registry[0] );
-	enqueue("Breakfast", &mt3, registry[0] );
-
-	enqueue("Lunch", &mt4, registry[1] );
-	enqueue("Lunch", &mt5, registry[1] );
-	enqueue("Lunch", &mt6, registry[1] );
-
-	enqueue("Dinner", &mt7, registry[2] );
-	enqueue("Dinner", &mt8, registry[2] );
-	enqueue("Dinner", &mt9, registry[2] );
-
-	enqueue("Bar", &mt10, registry[3] );
-	enqueue("Bar", &mt11, registry[3] );
-	enqueue("Bar", &mt12, registry[3] );
-
-	result = enqueue("Breakfast", &mt1, registry[0] );
-	if(result == 0 && registry[0]->tail == 4){
-		printf("The queue is full\n");
-		printf("Test Case C - Result Fail\n");
-	}
-
-//	dequeue("Breakfast", &mt_(0+1), registry[0] );
-
-//	int result;
-//
-//	for(int i=0; i<4; i++){
-//		dequeue("Breakfast", &mt(i+1), registry[0] );
-//		dequeue("Lunch", &mt(i+4), registry[1] );
-//		dequeue("Dinner", &mt(i+7), registry[2] );
-//		dequeue("Bar", &mt(i+9), registry[3]);
-//	}
-
-	free(breakfast.buffer);
-	free(lunch.buffer);
-	free(dinner.buffer);
-	free(bar.buffer);
-	return 0;
+	if(test[0]) {printf("Test Case: A - Result: Success\n");}
+	else {printf("Test Case: A - Result: Fail\n");}
+	if(test[1]) {printf("Test Case: B - Result: Success\n");}
+	else {printf("Test Case: B - Result: Fail\n");}
+	if(test[2]) {printf("Test Case: C - Result: Success\n");}
+	else {printf("Test Case: C - Result: Fail\n");}
+	if(test[3]) {printf("Test Case: D - Result: Success\n");}
+	else {printf("Test Case: D - Result: Fail\n");}
+	printf("=============================\n");
+	return EXIT_SUCCESS;
 }
+//=============================================================================
