085d95a7440a18e61c6564b225fa4dc19a598c29
lionelhan <d.bcooper@hotmail.com>
2019-12-09 13:10:48 -0800

    final push

--- assignment2/part1.c
+++ assignment2/part1.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
 
 
 pid_t forkChild(char *command, char **arguments){
