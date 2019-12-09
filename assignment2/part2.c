7b4064f2e0dfd5909eace5806048e811d123b47f
lionelhan <d.bcooper@hotmail.com>
2019-12-09 11:13:37 -0800

    final push

--- assignment2/part2.c
+++ assignment2/part2.c
@@ -9,13 +9,13 @@
 
 void myHandler(int mySignal){
 
-	printf("Child Process: %d waiting - Received signal: %d\n", getpid(),mySignal);
-	sigset_t set;
-	sigemptyset(&set);
-	sigaddset(&set, SIGUSR1);
-	sigprocmask(SIG_SETMASK, &set, NULL);
-	int sig;
-	sigwait(&set, &sig);
+	// printf("Child Process: %d waiting - Received signal: %d\n", getpid(),mySignal);
+	// sigset_t set;
+	// sigemptyset(&set);
+	// sigaddset(&set, SIGUSR1);
+	// sigprocmask(SIG_SETMASK, &set, NULL);
+	// int sig;
+	// sigwait(&set, &sig);
 }
 
 
@@ -45,7 +45,7 @@ pid_t forkChild(char *command, char **arguments){
 void singler(int count, int signal, pid_t *pid){
 	int i;
 	for(i=1; i<=count; i++){
-		printf("receiving signal %d\n", signal);
+		printf("child receiving signal %d\n", signal);
 		kill(pid[i], signal);
 	}
 }
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]){
 	char* token = NULL;
 	pid_t pid[10];
 	FILE *fp;
-	int count=0, i, j=0, wstatus, countArg = 0, a=0;
+	int count=0, i, j=0, k, wstatus, countArg = 0, a=0;
 
 	if(argc != 2){
 		printf("The program need exact one file to execute, please try again\n");
@@ -93,26 +93,21 @@ int main(int argc, char *argv[]){
 			countArg = 0;
 			j++;
 		}
-		printf("sending signal to pause all the processes!\n");
+		
+		sleep(3);
+		printf("sending SIGUSR1 signal to pause all the processes!\n");		// sigusr signal to pause all the children
     	singler(j, SIGUSR1, pid);
    		sleep(3);
-   		printf("sending signal to restart all the processes!\n");
-   		singler(j, SIGUSR1, pid);
-   		sleep(3);
-   		printf("sending signal to stop all the processes!\n");
+   		printf("sending SIGSTOP signal to stop all the processes!\n");
    		singler(j, SIGSTOP, pid);
    		sleep(4);
-   		printf("sending signal to continue all the processes!\n");
+   		printf("sending SIGCONT signal to continue all the processes!\n");
    		singler(j, SIGCONT, pid);
 		sleep(3);
-		printf("terminating all the processes!\n");
-		singler(j, SIGINT, pid);
-		sleep(3);
+		
 
-		for(int k=0; k<j; k++){
-
-			printf("pid is waiting %d\n", pid[k]);
-		 	waitpid(pid[k], &wstatus, WNOHANG);
+		for(k=0; k<j; k++){
+		 	waitpid(pid[k], &wstatus, WUNTRACED);
 			}
 
 	}
