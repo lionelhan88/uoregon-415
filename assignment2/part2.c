06c2f405edc9abdd44926a9d9d7621bd5d6cee2e
lionelhan <d.bcooper@hotmail.com>
2019-12-09 11:48:33 -0800

    final push

--- assignment2/part2.c
+++ assignment2/part2.c
@@ -10,111 +10,141 @@
 #include <fcntl.h> 
 #include <signal.h>
 
+void sighandler(int);
 
-void myHandler(int mySignal){
+int main(int argc, char *argv[]) {
+    //initial the varibales.
+    FILE *input = NULL;
+    char buf[1024];
+    size_t nread;
+    char *line;
+    size_t bufsize = 1024;
+    int count_line = 0;
+    int k;
+    const char *space = " ";
+    char *token;
+    char **ptr;
+	size_t ptrSize = 20;
 
-	printf("Child Process: %d waiting - Received signal: %d\n", getpid(),mySignal);
-	sigset_t set;
-	sigemptyset(&set);
-	sigaddset(&set, SIGUSR1);
-	sigprocmask(SIG_SETMASK, &set, NULL);
-	int sig;
-	sigwait(&set, &sig);
-}
+    if (argc == 2) {  //open the input file
+        input = fopen(argv[1], "r");
+        if (!input) {//error handling
+            printf("%s not exiting\n", argv[1]);
+            exit(1);
+        }
+    }
+
+    line = (char *)malloc(bufsize * sizeof(char));
+    while((nread = getline(&line, &bufsize, input)) != -1){
+    	count_line += 1; //count the line number in the input file
+    }
+    free(line);
+    fclose(input);
 
 
-pid_t forkChild(char *command, char **arguments){
-	pid_t pid;
-	struct sigaction act;
-	act.sa_handler = &myHandler;
-	sigset_t set;
-	sigemptyset(&set);
-	sigaddset(&set, SIGUSR1);
-	sigprocmask(SIG_BLOCK, &set, NULL);
+    //open the file again
+    line = (char *)malloc(bufsize * sizeof(char));
+    pid_t pid[count_line], w;
+    int wstatus;
+    input = fopen(argv[1], "r");
 
-	pid = fork();
-	if(pid == -1){
-		printf("Error, failed to fork \n");
-	}else if(pid == 0){
+    for(int index = 0 ; index < count_line; index++){
+	    ptr = (char **)malloc(ptrSize * sizeof(char*));
+        memset(ptr, 0, sizeof(ptr));
+	 	getline(&line, &bufsize, input);
 
-		printf("child pid is %d\n", getpid());
-		sigaction(SIGUSR1,&act,NULL);
-		execvp( command, arguments);
-		sleep(2);
-		_exit(0);
-	}
-	return pid;
-}
-
-void singler(int count, int signal, pid_t *pid){
-	int i;
-	for(i=1; i<=count; i++){
-		printf("receiving signal %d\n", signal);
-		kill(pid[i], signal);
-	}
-}
-
-int main(int argc, char *argv[]){
-
-	size_t size, line;
-	char *text= NULL;
-	char* token = NULL;
-	pid_t pid[10];
-	FILE *fp;
-	int count=0, i, j=0, wstatus, countArg = 0, a=0;
-
-	if(argc != 2){
-		printf("The program need exact one file to execute, please try again\n");
-		exit(-1);
-	}
-
-	if(strstr(argv[1], ".txt")){
-
-		fp = fopen(argv[1], "r");
-		while((line = getline(&text, &size, fp)) != -1){			// while loop
-
-			for(i=0; i<strlen(text); i++){
-				if(text[i] == ' '){
-	 				countArg++;
- 				}
-			}
-			count = countArg + 1; 									// count how many arguments each line has in the file
-
-			char *array[count];
-			for(i=0; i <= count ; i++){
-				array[i] = NULL;									// initialize an array pointer
-			}
-
-			token = strtok(text, " \r\n");
-			for(i=0; i < count ; i++){
-				array[i] = token;									// store token into the array pointer
-				token = strtok(NULL, " \r\n");
-			}
-
-		 	pid[a] = forkChild(array[0], array);					// fork child processes
-		 	a++;
-			count = 0;
-			countArg = 0;
-			j++;
+	 	// *ptr = (char *)malloc(count_line * sizeof(char));
+	 	strtok(line , "\n");
+	 	int arg_index = 1;
+	 	token = NULL;
+	 	token = strtok(line, space);
+	 	ptr[0] = (char*)malloc(strlen(token) + 1);
+	 	strcpy(ptr[0],token);
+	 	//printf("%s\n", ptr[0]);
+	 	token = strtok(NULL, space);
+	 	while(token != NULL){
+	 		//printf("%s\n", token);
+	 		ptr[arg_index] = malloc(strlen(token)+1);
+	 		//printf("123 %s\n", ptr[arg_index]);
+	 		strcpy(ptr[arg_index++],token);
+	 		token = strtok(NULL, space);
+	 	}
+        ptr[arg_index++] = NULL;
+    	pid[index] = fork();
+		if(pid[index] < 0){
+			printf("failed to fork\n");
 		}
-		printf("sending signal to pause all the processes!\n");
-    	singler(j, SIGUSR1, pid);
-   		sleep(3);
-   		printf("sending signal to stop all the processes!\n");
-   		singler(j, SIGSTOP, pid);
-   		sleep(4);
-   		printf("sending signal to continue all the processes!\n");
-   		singler(j, SIGCONT, pid);
-		sleep(3);
+    	if(pid[index] == 0){
+            struct sigaction sa; //initial the sigaction struct
+            sa.sa_handler = sighandler; //set handler 
+            sigemptyset(&sa.sa_mask); //empty and clean the set 
+            sa.sa_flags = 0;  // set the flag as default
+            sigaction(SIGUSR1, &sa, NULL); //send the SIGUSR1
+            sigset_t sigSet; //initial set for signal
+            sigemptyset(&sigSet); //empty all sig in sigset
+    		printf("the pid is %d\n", getpid());
+            sigaddset(&sigSet, SIGUSR1);
+            //sleep(3);
+            int sig;
+            sigwait(&sigSet, &sig);
+            printf("Child Process: %d  Received SIGUSR1 signal\n", getpid());
+            fclose(input);
+            free(line);
+    		execvp(ptr[0],ptr);
+            for(k = 0; k < (arg_index); k++){ //free the ptr array
+                free(ptr[k]);
+            }
+            free(ptr);
+    		_exit(-1);
+    	}
+    	for(k = 0; k < (arg_index); k++){
+    		free(ptr[k]);
+    	}
+    	free(ptr);
+    }
 
-		for(int k=0; k<j; k++){
+    free(line);
 
-		 	waitpid(pid[k], &wstatus, WUNTRACED);
-			}
+    sleep(3);
+    for(int in = 0 ; in < count_line; in++){
+        if(kill(pid[in], SIGUSR1) != 0){ //send the SIGUSR1 second time to sighandler
+            printf("failed to send SIGUSR1 signal to pid: %d\n", pid[in]);
+        }
+    }
+    sleep(5);
+    for(int k = 0 ; k < count_line; k++){
+        if(kill(pid[k], SIGSTOP) != 0){ //send the SIGSTOP second time to sighandler
+            printf("failed to send SIGSTOP signal to pid: %d\n", pid[k]);
+        }
+        else{
+            printf("%d Received SIGSTOP signal\n", pid[k]);
+        }
+    }
 
-	}
-	free(text);
-	fclose(fp);
+    printf("\n");
+    sleep(5);
+    for(int l = 0 ; l < count_line; l++){
+        if(kill(pid[l], SIGCONT) != 0){ //send the SIG second time to sighandler
+            printf("failed to send SIGCONT signal to pid: %d\n", pid[l]);
+        }
+        else{
+            printf("%d Received SIGCONT signal\n", pid[l]);
+        }
+    }
 
-	return 0;
+    sleep(3);
+    for(int m = 0 ; m < count_line; m++){
+       //kill(pid[m], SIGINT);
+        w = waitpid(pid[k], &wstatus , WUNTRACED);
+    }
+
+
+    fclose(input);
+    printf("All child processes joined. Exiting\n");
+    return 0;
+}
+
+void sighandler(int signum)
+{   
+    //do nothing but will let pid wait 
 }
