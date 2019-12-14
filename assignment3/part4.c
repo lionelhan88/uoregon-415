0c7c4b7ca98eaeb72a4bcad7edf4a75310be27b2
lionelhan <d.bcooper@hotmail.com>
2019-12-13 20:04:39 -0800

    final push

--- assignment3/part4.c
+++ assignment3/part4.c
@@ -90,27 +90,28 @@ int enqueue(struct topicQueue *tpQueue, char *URL, char *Caption, int pubID){
 
 
 int getEntry(struct topicQueue *tpQueue, int lastEntry, struct topicEntry *temp){
-	if(tpQueue->head == tpQueue->tail || lastEntry == tpQueue->head || lastEntry == 0){
-		return 0;
+	if(tpQueue->head == tpQueue->tail || lastEntry == tpQueue->head 				// return 0 if the queue is empty 
+			|| lastEntry == 0){														// or the entry has been deleted
+		return 0;																	// or the entry is the newest
 	}
 	z+=1;
-	for (int j = tpQueue->tail; j < tpQueue->head; j++){
-		if (tpQueue->entry[j].entryNum == lastEntry){
+	for (int j = tpQueue->tail; j < tpQueue->head; j++){							// copy last entry +1 data to the temp buffer 
+		if (tpQueue->entry[j].entryNum == lastEntry){								// if the last entry is in the queue
 			tepp[z].entryNum = tpQueue->entry[(j+1)].entryNum;
 			tepp[z].timeStamp = tpQueue->entry[(j+1)].timeStamp;
 			tepp[z].pubID = tpQueue->entry[(j+1)].pubID;
 			strcpy(tepp[z].topName, tpQueue->name);
 			strcpy(tepp[z].photoURL, tpQueue->entry[(j+1)].photoURL);
 			strcpy(tepp[z].photoCaption, tpQueue->entry[(j+1)].photoCaption);
-			return 1;
-		}else if(lastEntry <= tpQueue->tail){
-			tepp[z].entryNum = tpQueue->entry[tpQueue->tail].entryNum;
+			return 1;																// return 1 for the success
+		}else if(lastEntry <= tpQueue->tail){										// copy last entry +1 data to the temp buffer 
+			tepp[z].entryNum = tpQueue->entry[tpQueue->tail].entryNum;				// if last entry isn't in the queue
 			tepp[z].timeStamp = tpQueue->entry[tpQueue->tail].timeStamp;
-			tepp[z].pubID = tpQueue->entry[tpQueue->tail].pubID;
-			strcpy(tepp[z].topName, tpQueue->name);
+			tepp[z].pubID = tpQueue->entry[tpQueue->tail].pubID;					// ie. if last entry is 1 and entry 1, 2,
+			strcpy(tepp[z].topName, tpQueue->name);									// isn't in the queue, then take entry 3
 			strcpy(tepp[z].photoURL, tpQueue->entry[tpQueue->tail].photoURL);
 			strcpy(tepp[z].photoCaption, tpQueue->entry[tpQueue->tail].photoCaption);
-			return tepp[z].entryNum;
+			return tepp[z].entryNum;												// return the entry number 
 		}
 	}
 	return 0;
@@ -120,24 +121,23 @@ int getEntry(struct topicQueue *tpQueue, int lastEntry, struct topicEntry *temp)
 int dequeue(struct topicQueue *tpQueue){
 
 	if(tpQueue->head == tpQueue->tail){
-		return 0;
-	}
-	tpQueue->tail += 1;
+		return 0;																	// return 0 if the queue is empty
+	}	
+	tpQueue->tail += 1;																// move the tail towards to the head
 	return 1;
 
 } // dequeue()
 
 int cleanUp(struct topicEntry *temp, struct topicQueue *tpQueue){
-	gettimeofday(&temp->timeStamp,NULL);
+	gettimeofday(&temp->timeStamp,NULL);											// get time stamp, and compare it with each entry's delta
 	int diff, result;
 
 	if (tpQueue->head == tpQueue->tail){
-	
-		return 0;
+		return 0;																	// return 0 if the queue is empty
 	}
 	for (int j = 0; j < tpQueue->head; ++j){
 		diff = (int)(temp->timeStamp.tv_sec) - (int)(tpQueue->entry[j].timeStamp.tv_sec);
-		if (diff > delta){
+		if (diff > delta){															// call dequeue function if the entry is too old
 			result = dequeue(tpQueue);
 			printf("Dequeue is called, result %d\n", result);
 			printf("Topic queue head: %d, tail: %d\n", tpQueue->head, tpQueue->tail);
@@ -150,8 +150,8 @@ int cleanUp(struct topicEntry *temp, struct topicQueue *tpQueue){
 	return 1;
 } // cleanUp()
 
-void initThreadEnq(char *Q_id){
-	
+void initThreadEnq(char *Q_id){														// initialize struct that will be used
+																					// in publisher
 	for (i = 0; i < MAXENTRIES; ++i){
 		threadEnq[i].lockPos = 0;
 		strcpy(threadEnq[i].Q_id, Q_id);
@@ -160,8 +160,8 @@ void initThreadEnq(char *Q_id){
 } // initialize threadEnq
 
 
-void initThreadDeq(char *Q_id){
-	
+void initThreadDeq(char *Q_id){														// initialize struct that will be used
+																					// in subscriber
 	for (i = 0; i < MAXENTRIES; ++i){
 		threadDeq[i].lockPos = 0;
 		strcpy(threadDeq[i].Q_id, Q_id);
@@ -171,8 +171,8 @@ void initThreadDeq(char *Q_id){
 } // initialize threadDeq
 
 
-void initPtred(){
-	
+void initPtred(){																	// initialize struct that will be used
+																					// for multi-thread															
 	for (int i = 0; i < MAXENTRIES; ++i){
 		pub[i].flag = 0;
 		sub[i].flag = 0;
@@ -184,19 +184,19 @@ void* publisher(void* arg){
 	struct threadEnq *threadEnq =(struct threadEnq *) arg;
 
 	pthread_mutex_lock(&condition_mutex);
-	pthread_cond_wait(&cond, &condition_mutex);
+	pthread_cond_wait(&cond, &condition_mutex);										// pthread conditional wait, waiting signal from main
 	pthread_mutex_unlock(&condition_mutex);
 
-	pthread_mutex_lock(&mutex[threadEnq->lockPos]); 
+	pthread_mutex_lock(&mutex[threadEnq->lockPos]); 								// lock the critical section for publisher
 	threadEnq->result = enqueue(&Registry[threadEnq->regisNum], threadEnq->topicEntry.photoURL,
 			threadEnq->topicEntry.photoCaption, threadEnq->topicEntry.pubID);
 	printf("publisher result: %d, Topic Queue head: %d, tail: %d\n", threadEnq->result, Registry[threadEnq->regisNum].head,
 			Registry[threadEnq->regisNum].tail);
 	printf("-------------------------------------------------------------------------\n");
-	pthread_mutex_unlock(&mutex[threadEnq->lockPos]);
+	pthread_mutex_unlock(&mutex[threadEnq->lockPos]);								// unlock the publisher critical section
 	for (int i = 0; i < MAXENTRIES; ++i){
 		if (pub[i].thread_id == pthread_self()){
-			pub[i].flag = 0;
+			pub[i].flag = 0;														// change the publisher thread flag
 			break;
 		}
 	}
@@ -206,25 +206,21 @@ void* publisher(void* arg){
 
 void* subscriber(void* arg){
 	struct threadEnq *threadDeq =(struct threadEnq *) arg;
-	
 
 	pthread_mutex_lock(&condition_mutex);
-	pthread_cond_wait(&cond, &condition_mutex);
+	pthread_cond_wait(&cond, &condition_mutex);										// pthread conditional wait, waiting signal from main
 	pthread_mutex_unlock(&condition_mutex);
 
-	
-
-	pthread_mutex_lock(&mutex[threadDeq->lockPos]);
+	pthread_mutex_lock(&mutex[threadDeq->lockPos]);									// lock the critical section for subscriber
 	threadDeq->result = getEntry(&Registry[threadEnq->regisNum], threadDeq->lastEntry, &temp1);
 	printf("subscriber result: %d\n", threadDeq->result);
 	printf("-------------------------------------------------------------------------\n");
 	
-	
-	pthread_mutex_unlock(&mutex[threadDeq->lockPos]);
+	pthread_mutex_unlock(&mutex[threadDeq->lockPos]);								// unlock the subscriber critical section
 	
 	for (int i = 0; i < MAXENTRIES; ++i){
 		if (sub[i].thread_id == pthread_self()){
-			sub[i].flag = 0;
+			sub[i].flag = 0;														// change the subscriber thread flag
 			break;
 		}
 	}
@@ -235,19 +231,19 @@ void* pthread_cleanUp(void* arg){
 	struct threadEnq *threadCle =(struct threadEnq *) arg; 
 
 	pthread_mutex_lock(&condition_mutex);
-	pthread_cond_wait(&cond, &condition_mutex);
+	pthread_cond_wait(&cond, &condition_mutex);										// pthread conditional wait, waiting signal from main
 	pthread_mutex_unlock(&condition_mutex);
 
 	while(check == 1){
-		pthread_mutex_lock(&lock[threadCle->lockPos]);
+		pthread_mutex_lock(&lock[threadCle->lockPos]);								// lock the critical section for clean up
 		check = cleanUp(&temp1, &Registry[threadCle->regisNum]);
-		pthread_mutex_unlock(&lock[threadCle->lockPos]);
-		sched_yield();
+		pthread_mutex_unlock(&lock[threadCle->lockPos]);							// unlock the clean up critical section
+		sched_yield();																// yeild to publisher and subscriber
 	}
 	return NULL;
 } // pthread_cleanUp()
 
-void initRgistry(){
+void initRgistry(){																	// Registry initialization
 	for (int i = 0; i < NUMOFBUFFER; ++i){
 		Registry[i].head = 0;
 		Registry[i].tail = 0;
@@ -257,7 +253,7 @@ void initRgistry(){
 	}
 }
 
-void createQueue(int id, char *name, int len){
+void createQueue(int id, char *name, int len){										// Queue initialization
 	strcpy(Registry[id].name, name);
 	Registry[id].length = len;
 	Registry[id].entry = Entries[id];
@@ -276,11 +272,11 @@ int main(int argc, char* argv[]){
 	struct topicEntry temp;
 	int pQuery[MAXENTRIES], sQuery[MAXENTRIES];
 
-	if(argc != 2){
+	if(argc != 2){																	// error handler for txt file missing
 		printf("The program need one txt file to execute, please try again\n");
 		printf("------------------------------------------------------------\n");
 		exit(-1);
-	}else if(strstr(argv[1],".txt")){
+	}else if(strstr(argv[1],".txt")){												// open and read input txt file
 		initRgistry();
 		initPtred();
 
@@ -295,7 +291,7 @@ int main(int argc, char* argv[]){
 			}
 			tk1_cnt = 0;
 
-			if (strcmp(command[0], "create") == 0){
+			if (strcmp(command[0], "create") == 0){									// "create topic" command
 				printf("-------------------------------------------------------------------------\n");
 				printf("Activating queue\n");
 				createQueue(atoi(command[2]), command[3], atoi(command[4]));
@@ -303,10 +299,10 @@ int main(int argc, char* argv[]){
 				initThreadDeq(command[3]);
 				actQueue += 1;
 			} 
-			if(strcmp(command[0], "add") == 0){
+			if(strcmp(command[0], "add") == 0){										// "add publisher/subscriber" command
 
-				if (strcmp(command[1], "publisher") == 0){
-					actPub+=1;
+				if (strcmp(command[1], "publisher") == 0){							
+					actPub+=1;														// varible that track act-publishers
 					strcpy(pubFilename, strtok_r(command[2], "\"", &command[2]));
 					if (strlen(pubFilename) <= 1 || !(strstr(pubFilename,".txt") ) ){
 						printf("Publisher filename error, please try again\n");
@@ -316,7 +312,7 @@ int main(int argc, char* argv[]){
 						return -1;
 					}else{
 						FILE *fpPub;
-						fpPub = fopen(pubFilename, "r");
+						fpPub = fopen(pubFilename, "r");							// open publisher txt file 
 						while( (line = getline(&pubText, &size, fpPub)) != -1){
 							dummy1 = pubText;
 							while(pubToken = strtok_r(dummy1, " ", &dummy1)){
@@ -324,7 +320,7 @@ int main(int argc, char* argv[]){
 								tk2_cnt += 1;
 							}
 							tk2_cnt = 0;
-							if (strcmp(pubcmd[0], "put") == 0){
+							if (strcmp(pubcmd[0], "put") == 0){						// publisher "put" command
 								cur = (atoi(pubcmd[1]) - 1);
 								threadEnq[pub_cnt].topicEntry.pubID = cur;
 								threadEnq[pub_cnt].regisNum = cur;
@@ -332,13 +328,13 @@ int main(int argc, char* argv[]){
 								strcpy(threadEnq[pub_cnt].topicEntry.photoCaption, pubcmd[3]);
 								pub_cnt += 1;
 
-							}else if(strcmp(pubcmd[0], "stop") == 0){
+							}else if(strcmp(pubcmd[0], "stop") == 0){				// publisher "stop" command
 								printf("Ends of publisher file: %s\n", pubFilename);
 								printf("-------------------------------------------------------------------------\n");
 								fclose(fpPub);
 								free(pubText);
 								break;
-							}else if(strcmp(pubcmd[0], "sleep") == 0){
+							}else if(strcmp(pubcmd[0], "sleep") == 0){				// publisher "sleep" command
 								int sleepTime = atoi(pubcmd[1]) / 1000;
 								sleep(sleepTime);
 							}
@@ -346,7 +342,7 @@ int main(int argc, char* argv[]){
 
 						for(i=0; i<MAXENTRIES; i++){
 							if(pub[i].flag == 0){
-								for(j=0; j<pub_cnt; j++){
+								for(j=0; j<pub_cnt; j++){							// create thread for publisher
 
 									pub[i].flag = 1;
 									threadEnq[j].topicEntry.pubID = pub[i].thread_id;
@@ -359,16 +355,16 @@ int main(int argc, char* argv[]){
 						}
 					}
 				}else if(strcmp(command[1], "subscriber") == 0){
-					actSub += 1;
+					actSub += 1;													// varible that track act-subscriber
 					subFile += 1;
 					strcpy(subFilename, strtok_r(command[2], "\"", &command[2]));
-					if (strlen(subFilename) <= 1 || !(strstr(subFilename,".txt") ) ){
+					if (strlen(subFilename) <= 1 || !(strstr(subFilename,".txt") ) ){	
 						printf("Subscriber filename error, please try again\n");
 						printf("-------------------------------------------------------------------------\n");
 						fclose(fp);
 						free(text);
 						return -1;
-					}else{
+					}else{															// open subscriber txt file 
 						FILE *fpSub;
 						fpSub = fopen(subFilename, "r");
 						while( (line = getline(&subText, &size, fpSub)) != -1){
@@ -377,8 +373,8 @@ int main(int argc, char* argv[]){
 								subcmd[tk3_cnt] = subToken;
 								tk3_cnt += 1;
 							}
-							tk3_cnt = 0;
-							if (strcmp(subcmd[0], "get") == 0){
+							tk3_cnt = 0;	
+							if (strcmp(subcmd[0], "get") == 0){						// subscriber "get" command
 								ruc = atoi(subcmd[1])-1;
 								threadDeq[sub_cnt].topicEntry.pubID = ruc;
 								threadDeq[sub_cnt].regisNum = ruc;
@@ -386,20 +382,20 @@ int main(int argc, char* argv[]){
 								sub_cnt += 1;
 								subTable += 1;
 
-							}else if(strcmp(subcmd[0], "stop") == 0){
+							}else if(strcmp(subcmd[0], "stop") == 0){				// subscriber "stop" command
 								printf("Ends of subscribers file: %s\n", subFilename);
 								printf("-------------------------------------------------------------------------\n");
 								fclose(fpSub);
 								free(subText);
 								break;
-							}else if(strcmp(subcmd[0], "sleep") == 0){
+							}else if(strcmp(subcmd[0], "sleep") == 0){				// subscriber "sleep" command
 								int sleepTime = atoi(subcmd[1]) / 1000;
 								sleep(sleepTime);
 							}
 						}
 
 						sleep(1);
-						for(i=0; i<MAXENTRIES; i++){
+						for(i=0; i<MAXENTRIES; i++){								// create thread for subscriber
 							if(sub[i].flag == 0){
 								for(j=0; j<sub_cnt; j++){
 									sub[i].flag = 1;
@@ -415,14 +411,14 @@ int main(int argc, char* argv[]){
 			} 
 
 			if(strcmp(command[0], "query") == 0){
-				if(strcmp(command[1], "topics\n") == 0){
+				if(strcmp(command[1], "topics\n") == 0){							// "query topics" command
 					printf("There are %d queues activating \n", actQueue );
 					for (int i = 0; i < actQueue; ++i){
 						printf("Queue id %d, name: %s, length: %d\n", Registry[i].id, Registry[i].name, Registry[i].length );
 						printf("-------------------------------------------------------------------------\n");					 
 					}
 				}
-				if (strcmp(command[1], "publishers\n") == 0){
+				if (strcmp(command[1], "publishers\n") == 0){						// "query publishers" command
 					for (int i = 0; i < MAXENTRIES; ++i){
 						if(pub[i].flag == 1){
 							printf("Proxy thread %ld - type: Publisher\n", pub[i].thread_id);
@@ -430,7 +426,7 @@ int main(int argc, char* argv[]){
 						}	
 					}
 				}
-				if(strcmp(command[1], "subscribers\n") == 0){
+				if(strcmp(command[1], "subscribers\n") == 0){						// "query subscribers" command
 					for (int i = 0; i < MAXENTRIES; ++i){
 						if(sub[i].flag == 1){
 							printf("Proxy thread %ld - type: Subscriber \n", sub[i].thread_id);
@@ -440,17 +436,17 @@ int main(int argc, char* argv[]){
 				}
 			} 
 
-			if(strcmp(command[0], "delta") == 0){
+			if(strcmp(command[0], "delta") == 0){									// "delta" command
 				delta = atoi(command[1]);
 				printf("delta is %d\n", delta);
 				printf("-------------------------------------------------------------------------\n");
 			}
 
-			if(strcmp(command[0], "start\n") == 0 || strcmp(command[0], "start") == 0){
+			if(strcmp(command[0], "start\n") == 0 || strcmp(command[0], "start") == 0){	// "start" command
 				for (int k = 0; k < MAXENTRIES; ++k){	
 					if (pub[k].flag == 1){
 						pthread_mutex_lock(&condition_mutex);
-						pthread_cond_broadcast(&cond);
+						pthread_cond_broadcast(&cond);								// join publiser thread
 						pthread_mutex_unlock(&condition_mutex);
 						pthread_join(pub[k].thread_id, NULL);
 					}
@@ -460,7 +456,7 @@ int main(int argc, char* argv[]){
 				for (int k = 0; k < MAXENTRIES; ++k){	
 					if (pub[k].flag == 1){
 						pthread_mutex_lock(&condition_mutex);
-						pthread_cond_broadcast(&cond);
+						pthread_cond_broadcast(&cond);								// join subscriber thread
 						pthread_mutex_unlock(&condition_mutex);
 						pthread_join(sub[k].thread_id, NULL);
 
@@ -468,26 +464,26 @@ int main(int argc, char* argv[]){
 				}
 
 				for (int i = 0; i < actQueue; ++i){
-                	threadCle[i].regisNum = i;
+                	threadCle[i].regisNum = i;										// create clean up thread
                     pthread_create(&(cle.thread_id), NULL, pthread_cleanUp, &threadCle[i]);
                 }
 				
 				sleep(1);
 				pthread_mutex_lock(&condition_mutex);
-				pthread_cond_broadcast(&cond);
+				pthread_cond_broadcast(&cond);										// send continue signal to all the threads
 				printf("Start cleaning up the old entries\n");
 				printf("-------------------------------------------------------------------------\n");
-				pthread_mutex_unlock(&condition_mutex);
+				pthread_mutex_unlock(&condition_mutex);								// join clean up thread
 				pthread_join(cle.thread_id, NULL);
 			}
 		}
 		token = NULL;
-		fclose(fp);
+		fclose(fp);																	// close input txt file
 	}	
 
 	sleep(1);
 	int o = 0, v=0;
-	for(i=0; i<actSub; i++){
+	for(i=0; i<actSub; i++){														// write data to subscriber html file
 		x += 1;
 		snprintf(name, 12, "Sub_%d.html", x);
 		FILE *ptrFile = fopen( name, "w");
